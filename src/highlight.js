4b0c1d7b6c54961880a25191549040c8fa35af75
Horatiu Halmaghi <hhalmaghi@protonmail.com>
2021-05-05 19:01:43 -0400

    refactors highlights

--- src/highlight.js
+++ src/highlight.js
@@ -2,30 +2,95 @@ const grandSlalami = require('grand-slalami');
 
 // making a 'class' for highlights in the old-school way
 // cause i don't wanna have to complicate things further with babel
-let id;
-let gameEvent;
-let mlustard;
-let commentary;
-
-const generateCommentary = () => {
-  return grandSlalami.getComment({
-    gameEvent: gameEvent.data,
-    mlustard: mlustard,
-  });
-};
-
 const makeHighlight = (settings) => {
-  id = settings.id;
-  gameEvent = settings.gameEvent;
-  mlustard = settings.mlustard;
-  commentary = generateCommentary();
+  settings = settings || {};
 
-  return {
-    id,
-    gameEvent,
-    mlustard,
-    commentary,
-  }
+  let id;
+  let gameEvent;
+  let mlustard;
+  let commentary;
+  let maxDialogLen;
+  let dialogParts;
+  let curDialogPart;
+
+  const init = () => {
+    id = settings.id;
+    gameEvent = settings.gameEvent;
+    mlustard = settings.mlustard;
+    commentary = generateCommentary();
+    maxDialogLen = setMaxDialogLen();
+    dialogParts = [];
+    curDialogPart = 0;
+    breakIntoDialogParts();
+
+    return {
+      id,
+      gameEvent,
+      mlustard,
+      commentary,
+      dialogParts,
+      curDialogPart,
+    };
+  };
+
+  const generateCommentary = () => {
+    return grandSlalami.getComment({
+      gameEvent: gameEvent.data,
+      mlustard: mlustard,
+    });
+  };
+
+  // todo: do i wanna support mobile? then update this accordingly
+  const setMaxDialogLen = () => {
+    return 40;
+  };
+
+  const breakIntoDialogParts = () => {
+    // split by newlines (enforced pacing)
+    const pacingParts = commentary.split('\n');
+
+    pacingParts.forEach((p) => {
+      breakIntoParts(p);
+    });
+  };
+
+  // split each part into its animation parts by line length
+  const breakIntoParts = (text) => {
+    let parts = [[], []];
+    let unfilled = 0;
+    let len = 0;
+
+    text.split(' ').forEach((word) => {
+      let wordLen = word.length;
+
+      if ((len + wordLen) > maxDialogLen) {
+        len = 0;
+
+        if (unfilled === 1) {
+          dialogParts.push(makeSentences(parts));
+          parts = [[], []];
+          unfilled = 0;
+        } else {
+          unfilled = 1;
+        }
+      }
+
+      if ((len + wordLen) <= maxDialogLen) {
+        parts[unfilled].push(word);
+        len += wordLen + 1;
+      }
+    });
+
+    dialogParts.push(makeSentences(parts));
+  };
+
+  const makeSentences = (parts) => {
+    return parts.map((words) => {
+      return words.join(' ');
+    });
+  };
+
+  return init();
 };
 
 module.exports = {
